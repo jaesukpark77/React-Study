6ef8d730b3337341d94781a55ce58e219991c972
Jaeseok Park <jaesukpark77@gmail.com>
2023-12-18 23:03:49 +0900

    Feat. 생활코딩_2. React Router DOM - 실습 준비

--- 생활코딩/2. React Router DOM/react-router-dom-example/src/index.js
+++ 생활코딩/2. React Router DOM/react-router-dom-example/src/index.js
@@ -3,9 +3,41 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  )
+}
+
+function Topics() {
+  return (
+    <div>
+      <h2>Topics</h2>
+      Topics...
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div>Hello React Router DOM</div>
+    <div>
+      <h1>React Router DOM example</h1>
+      <Home></Home>
+      <Topics></Topics>
+      <Contact></Contact>
+    </div>
   )
 }
 
