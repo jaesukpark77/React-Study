2d765dfd11dffef53288a63cc7f283e175e6942d
Jaeseok Park <jaesukpark77@gmail.com>
2023-12-21 00:17:20 +0900

    Add. 생활코딩_2. React Router DOM - styled components 설정

--- 생활코딩/2. React Router DOM/react-router-dom-example/src/App.js
+++ 생활코딩/2. React Router DOM/react-router-dom-example/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const SimpleButton = styled.button`
+  color:white;
+  background-color:green;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SimpleButton>Simple</SimpleButton>
     </div>
   );
 }
