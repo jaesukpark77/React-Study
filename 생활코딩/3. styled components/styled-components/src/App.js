177c3d998c8b0049eb8f9caede222be7ed4f9066
Jaeseok Park <jaesukpark77@gmail.com>
2023-12-28 16:44:44 +0900

    Add. 생활코딩_3. styled components - general method

--- 생활코딩/3. styled components/styled-components/src/App.js
+++ 생활코딩/3. styled components/styled-components/src/App.js
@@ -12,11 +12,22 @@ const LargeButton = styled(SimpleButton)`
   font-size: 50px;
 `;
 
+// general method
+const ReactButton = props => {
+  return <button className={props.className}>{props.children}</button>
+}
+
+const ReactLargeButton = styled(ReactButton)`
+  font-size: 50px;
+`;
+
 function App() {
   return (
     <div>
       <SimpleButton>Simple</SimpleButton>
       <LargeButton>Large</LargeButton>
+      <ReactButton>React</ReactButton>
+      <ReactLargeButton>React Large</ReactLargeButton>
     </div>
   );
 }
